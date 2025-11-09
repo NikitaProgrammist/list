7716f0c69c07ac649e130008de394e49196451f9
NikitaProgrammist <nikita.govorov.nikita@yandex.ru>
2025-11-10 01:45:47 +0300

    difference between my std and boost lists

--- headers/time_memory.h
+++ headers/time_memory.h
@@ -1,10 +1,15 @@
 #ifndef TIME_MEMORY_H
 #define TIME_MEMORY_H
 
+#include <boost/pool/pool_alloc.hpp>
+#include <boost/container/list.hpp>
+
 void check();
-void stdInsert(std::list<double> * l1);
+void stdInsert(std::list<double, boost::fast_pool_allocator<double>> * l1);
+void boostInsert(boost::container::list<double, boost::fast_pool_allocator<double>> * l3);
 void myInsert(List * l2);
-void stdDelete(std::list<double> * l1);
+void stdDelete(std::list<double, boost::fast_pool_allocator<double>> * l1);
+void boostDelete(boost::container::list<double, boost::fast_pool_allocator<double>> * l3);
 void myDelete(List * l2);
 
 #endif // TIME_MEMORY_H
--- sources/time_memory.cpp
+++ sources/time_memory.cpp
@@ -3,34 +3,48 @@
 #include <time.h>
 #include <list>
 #include <new>
+#include <boost/pool/pool_alloc.hpp>
+#include <boost/container/list.hpp>
 
 #include "list.h"
 #include "time_memory.h"
 
 void check() {
-  std::list<double> l1 = {};
+  std::list<double, boost::fast_pool_allocator<double>> l1 = {};
+  boost::container::list<double, boost::fast_pool_allocator<double>> l3 = {};
   List * l2 = NULL;
   listInit(&l2);
   stdInsert(&l1);
+  boostInsert(&l3);
   myInsert(l2);
   stdDelete(&l1);
+  boostDelete(&l3);
   myDelete(l2);
   listDestroy(l2);
 }
 
-void stdInsert(std::list<double> * l1) {
+void stdInsert(std::list<double, boost::fast_pool_allocator<double>> * l1) {
   time_t t0 = clock();
   for (int i = 0; i < 100000000; i++) {
-    l1->emplace_front(rand() % 1000000);
+    l1->emplace_front(1);
   }
   time_t t1 = clock();
   printf("std: %lg\n", (double) (t1 - t0) / CLOCKS_PER_SEC);
 }
 
+void boostInsert(boost::container::list<double, boost::fast_pool_allocator<double>> * l3) {
+  time_t t0 = clock();
+  for (int i = 0; i < 100000000; i++) {
+    l3->emplace_front(1);
+  }
+  time_t t1 = clock();
+  printf("boost: %lg\n", (double) (t1 - t0) / CLOCKS_PER_SEC);
+}
+
 void myInsert(List * l2) {
   time_t t2 = clock();
   for (int i = 0; i < 100000000; i++) {
-    listInsertRight(l2, 0, rand() % 1000000);
+    listInsertRight(l2, 0, 1);
   }
   time_t t3 = clock();
   size_t size = 0;
@@ -38,13 +52,22 @@ void myInsert(List * l2) {
   printf("my: %lg\n", (double) (t3 - t2) / CLOCKS_PER_SEC);
 }
 
-void stdDelete(std::list<double> * l1) {
+void stdDelete(std::list<double, boost::fast_pool_allocator<double>> * l1) {
   time_t t4 = clock();
   for (int i = 0; i < 100000000; i++) {
     l1->pop_front();
   }
   time_t t5 = clock();
   printf("std delete: %lg\n", (double) (t5 - t4) / CLOCKS_PER_SEC);
+}
+
+void boostDelete(boost::container::list<double, boost::fast_pool_allocator<double>> * l3) {
+  time_t t4 = clock();
+  for (int i = 0; i < 100000000; i++) {
+    l3->pop_front();
+  }
+  time_t t5 = clock();
+  printf("boost delete: %lg\n", (double) (t5 - t4) / CLOCKS_PER_SEC);
 }
 
 void myDelete(List * l2) {
